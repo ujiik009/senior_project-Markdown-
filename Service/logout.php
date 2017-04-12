abb0f8a48411c4d840c34261f9112a3f2854ddb0
apirat_BSRU <ikool009@hotmail.com>
2017-04-12 15:56:29 +0700

    Remove command bockQuote not work

--- Service/logout.php
+++ Service/logout.php
@@ -1,15 +1,13 @@
 <?php 
 	session_start();
 	session_destroy();
-	//unset($_SESSION['UID']);
-	//setcookie("UID", "", time()+1);
+	
 	$return = array();
 	if(setcookie("UID", "", time(), "/")){
 		$return['status'] = true;
 	}else{
 		$return['status'] = false;
 	}
-
 	echo json_encode($return);
 	
 
